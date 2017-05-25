535116360a85ad44e1d4cab665a2f74cbbfa37f4
Timi <oluwatimilehinadeniran@gmail.com>
2017-05-25 16:56:18 +0100

    Boilerplate code

--- app/src/main/java/com/example/oluwatimilehin/moviebuff/NetworkUtils.java
+++ app/src/main/java/com/example/oluwatimilehin/moviebuff/NetworkUtils.java
@@ -1,8 +1,19 @@
 package com.example.oluwatimilehin.moviebuff;
 
+import java.net.URL;
+
 /**
  * Created by Oluwatimilehin on 25/05/2017.
+ * oluwatimilehinadeniran@gmail.com.
  */
 
 public class NetworkUtils {
+
+    String mUrl;
+
+    private URL buildUrl(){
+       return null ;
+    };
+
+
 }
