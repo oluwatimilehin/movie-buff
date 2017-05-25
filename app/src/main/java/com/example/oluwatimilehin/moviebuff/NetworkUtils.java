e522fd61d520933e26a3df57f739cda41d5faf48
Timi <oluwatimilehinadeniran@gmail.com>
2017-05-25 18:42:30 +0100

    Define methods to parse JSON result

--- app/src/main/java/com/example/oluwatimilehin/moviebuff/NetworkUtils.java
+++ app/src/main/java/com/example/oluwatimilehin/moviebuff/NetworkUtils.java
@@ -1,6 +1,20 @@
 package com.example.oluwatimilehin.moviebuff;
 
+import android.content.Context;
+import android.net.Uri;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Created by Oluwatimilehin on 25/05/2017.
@@ -9,11 +23,115 @@ import java.net.URL;
 
 public class NetworkUtils {
 
-    String mUrl;
 
-    private URL buildUrl(){
-       return null ;
-    };
+    public static ArrayList<Movies> parseJson(String key) {
+        URL url = buildUrl(key);
+        String jsonResult = extractResult(url);
+        ArrayList<Movies> movies = new ArrayList<Movies>();
+
+        try {
+            JSONObject jsonObject = new JSONObject(jsonResult);
+            JSONArray  jsonArray = jsonObject.getJSONArray("results");
+            for(int i = 0; i < jsonArray.length(); i ++){
+                JSONObject results = jsonArray.getJSONObject(i);
+                String title = results.getString("title");
+                String year = results.getString("release_date");
+                String path = results.getString("poster_path");
+                movies.add(new Movies(title, year, path));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return  movies;
+
+    }
 
 
+    /**
+     * Construct the URL used to make a connection
+     *
+     * @param keyword
+     * @return
+     */
+    private static URL buildUrl(String keyword) {
+        Context context = null;
+        URL url = null;
+
+        final String MOVIE_SCHEME = "https";
+        final String MOVIE_AUTHORITY = "api.themoviedb.org";
+
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme(MOVIE_SCHEME)
+                .authority(MOVIE_AUTHORITY)
+                .appendPath("3")
+                .appendPath("movie")
+                .appendPath(keyword)
+                .appendQueryParameter("api_key", context.getString(R.string.api_key))
+                .build();
+
+        try {
+            url = new URL(builder.toString());
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    /**
+     * Method to return the JSON response from the API call
+     *
+     * @return
+     */
+    private static String extractResult(URL url) {
+
+        BufferedReader reader = null;
+        HttpURLConnection urlConnection = null;
+
+        String movieJSON = null;
+
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                return null;
+            }
+
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = reader.readLine();
+
+            while (line != null) {
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                return null;
+            }
+
+            movieJSON = buffer.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+        return movieJSON;
+    }
 }
