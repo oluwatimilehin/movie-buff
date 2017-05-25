a965d65b0ece65518daad7c5cdcd5c11fc34316e
Timi <oluwatimilehinadeniran@gmail.com>
2017-05-25 17:23:33 +0100

    Define base movies class

---
+++ app/src/main/java/com/example/oluwatimilehin/moviebuff/Movies.java
@@ -0,0 +1,32 @@
+package com.example.oluwatimilehin.moviebuff;
+
+/**
+ * Created by Oluwatimilehin on 25/05/2017.
+ * oluwatimilehinadeniran@gmail.com.
+ */
+
+public class Movies {
+
+    String mTitle;
+    String mImageUrl;
+    String mYear;
+
+    public Movies(String title, String year, String imageUrl){
+        mTitle = title;
+        mYear = year;
+        mImageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+
+    public String getYear() {
+        return mYear;
+    }
+
+}
