a06a65dbc5367a0dda56b509b2198591f24d696b
Timi <oluwatimilehinadeniran@gmail.com>
2017-07-05 12:14:58 +0100

    Add contract class

---
+++ app/src/main/java/com/example/oluwatimilehin/moviebuff/data/MovieContract.java
@@ -0,0 +1,10 @@
+package com.example.oluwatimilehin.moviebuff.data;
+
+/**
+ * Created by Oluwatimilehin on 05/07/2017.
+ * oluwatimilehinadeniran@gmail.com.
+ */
+
+public class MovieContract {
+
+}
