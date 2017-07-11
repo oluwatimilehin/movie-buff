3e8e6945c3d6a4d7f2a3bee28a863cbf6da1dec2
Timi <oluwatimilehinadeniran@gmail.com>
2017-07-11 11:31:56 +0100

    Centre details toolbar text

--- app/src/main/java/com/example/oluwatimilehin/moviebuff/details/DetailActivity.java
+++ app/src/main/java/com/example/oluwatimilehin/moviebuff/details/DetailActivity.java
@@ -17,6 +17,7 @@ import android.support.v4.app.NavUtils;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.Loader;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -76,6 +77,8 @@ public class DetailActivity extends MasterActivity {
         mToolbarText = (TextView) findViewById(R.id.toolbar_text);
         setSupportActionBar(mToolbar);
         mToolbarText.setText(R.string.movie_detail);
+        mToolbarText.setGravity(Gravity.NO_GRAVITY);
+        mToolbarText.setPadding(200, 0, 0 , 0);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
